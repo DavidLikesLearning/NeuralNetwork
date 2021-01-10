f94ad1573024f1ce7041040801b15970f32f10a8
David Jose Florez <sirdavid@stanford.edu>
2021-01-10 00:23:35 -0500

    started bacpropagation matrix calc

--- neuralNet.py
+++ neuralNet.py
@@ -16,6 +16,12 @@ def sig(x):
 	return 1/(1+np.exp(-x))
 	#we love broadcasting
 
+def derivAct(x, choice):
+	if choice==RELU:#derivative of Relu defined to be 0 at X<0, .5 @ x=0, 1 @ x>0
+		return np.where(x<np.zeros(len(x)),0,np.where(x==np.zeros(len(x)),.5,1))
+	else: #derivative of sigmoid
+		return (sig(x)*(1-sig(x)))
+
 def neuralNetInit(neuronsInlayers):
 	#that neurons in layers will carry a list of the count of neurons in each layer
 	#neuronsInLayers carries tuples throughout, the first member being the neuron count per layer
@@ -34,7 +40,7 @@ def neuralNetInit(neuronsInlayers):
 
 def forwardFeed(net, inputVec, keepZ=False):
 	acc = inputVec
-	record = [] #will keep track of all computation outputs after weights and biases before activation
+	record = [acc] #will keep track of all computation outputs after weights and biases before activation
 	for i in range(net['nLayers']-1):
 		acc = net['Weight'+str(i)]*acc + net['bias'+str(i)] 
 		record.append(acc)
@@ -42,7 +48,18 @@ def forwardFeed(net, inputVec, keepZ=False):
 			acc = relu(acc)
 		else:
 			acc = sig(acc)
-	return acc
+	if keepZ:
+		return acc, np.array(record) #only returns the Z records if learning, not when simply predicting
+	else:
+		return acc
 
+def cost(yHat, y): #defining cost as the sum of losses
+	return (1 / y.shape[1]) * sum([yHatI - yI for yHatI, yI in zip(yHat, y)])
 
-
+def derivs(net, record):
+	LAYERS = net['nLayers']
+	dlL = 1/LAYERS
+	daL = dlL#derivative of loss w respect to aL is 1
+	dzL = daL*derivAct(record[LAYERS-1],0)
+	dbL = 1/LAYERS*np.sum(dzL, axis=1, keepdims=True)
+	dwL = 1/LAYERS*dzL*np.transpose(net['act'+str(L-1)])
